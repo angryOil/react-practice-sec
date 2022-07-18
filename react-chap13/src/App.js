95206b0fc0e1ab2a58026b6f5708a697c6d59d71
angryOil <jipmj13@gmail.com>
2022-07-18 19:10:51 +0900

    스테이시 테스트

---
+++ react-chap13/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (<div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                스테이시 잘적용 되나요?
+                스테이시
+                스테이시 했습니다
+            </a>
+        </header>
+    </div>);
+}
+
+export default App;
